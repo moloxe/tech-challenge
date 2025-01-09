3a9f2facb5896ac5fefc96e6fa54f0191bbdcd18
Franco Moloche <>
2025-01-08 23:44:43 -0500

    feat: add rotate left button with icon to MatrixView component

--- src/app/rotate-matrix/_components/MatrixView/MatrixView.tsx
+++ src/app/rotate-matrix/_components/MatrixView/MatrixView.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import Image from "next/image";
+import RotateLeft from "@/assets/icons/RotateLeft.svg";
 
 type Props = {
   matrix: null | number[][];
@@ -29,6 +31,13 @@ const MatrixView: FC<Props> = ({ matrix, rotateLeft }) => {
         onClick={rotateLeft}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
+        <Image
+          src={RotateLeft}
+          alt="Rotate Left"
+          width={16}
+          height={16}
+          className="inline-block mr-2"
+        />
         Rotate Left
       </button>
       <div className="inline-block">
