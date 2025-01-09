165ad8e31cedae92cf79f9133f7aa20f13647652
Franco Moloche <>
2025-01-08 22:53:13 -0500

    feat: matrix input validation

---
+++ src/app/rotate-matrix/_utils/validateMatrix.ts
@@ -0,0 +1,17 @@
+export function validateMatrix(stringMatrix: string) {
+  const ERROR_MESSAGE = "Error parsing matrix";
+  const parsedMat = JSON.parse(stringMatrix);
+  const rows = parsedMat.length;
+  if (typeof rows !== "number") throw new Error(ERROR_MESSAGE);
+  const matrix: number[][] = new Array(rows).fill(0).map(() => []);
+  for (let iRow = 0; iRow < rows; iRow++) {
+    const col = parsedMat[iRow];
+    const cols = col.length;
+    if (cols !== rows) throw new Error(ERROR_MESSAGE);
+    for (let iCol = 0; iCol < cols; iCol++) {
+      const value = col[iCol];
+      matrix[iRow][iCol] = value;
+    }
+  }
+  return matrix;
+}
